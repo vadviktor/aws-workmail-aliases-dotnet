ea359cbec2120523246401fa50ba223613bb4f47
Viktor (Ikon) VAD <git@vadviktor.xyz>
2024-05-12 20:55:49 +0100

    Copy text to clipboard

---
+++ WebApp/wwwroot/app.js
@@ -0,0 +1,23 @@
+function copyToClipboard(text) {
+    return new Promise((resolve, reject) => {
+      navigator.permissions.query({name: "clipboard-write"}).then(result => {
+          if (result.state === "granted" || result.state === "prompt") {
+              // Clipboard access is granted. Use it to write to the clipboard
+              try {
+                  navigator.clipboard.writeText(text);
+                  console.log('Text copied to clipboard');
+                  resolve({'message': `${text} copied to clipboard`, 'type': 'info'});
+              } catch (err) {
+                  console.log('Failed to copy text: ', err);
+                  reject({'message': `Failed to copy text: ${err}`, 'type': 'error'});
+              }
+          } else {
+              console.log('Clipboard access denied');
+              reject({'message': 'Clipboard access denied', 'type': 'error'});
+          }
+      }).catch(err => {
+          console.log('Clipboard access denied: ', err);
+          reject({'message': `Clipboard access denied: ${err}`, 'type': 'error'});
+      });
+    });
+  }
